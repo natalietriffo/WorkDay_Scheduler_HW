99db5c0068a433f015b576cbaea9872b749feb09
Natalie Triffo <nattriffo@gmail.com>
2022-03-12 12:00:47 -0600

    final changes 3/12

--- javascript/script.js
+++ javascript/script.js
@@ -1,42 +1,44 @@
-$(document).ready(function(){
-var currentTime = moment().format("MMMM Do YYYY, H:mm:ss a");
-var currentHour = moment().hour();
-var currentDay = moment().format("MMMM Do, YYYY");
-
-currentDay = moment().format("MMMM Do YYYY, H:mm:ss a");
-$("#currentDay").text("Today's Date: " + currentDay)
-
-$(".saveBtn").on("click", function() {
-var textArea = $(this).attr('data-value');
-var userInput = $(textArea).val();
-localStorage.setItem("#" + textArea, userInput);
-console.log(textArea)
-console.log(userInput)
-});
-
-let timeBlock = ["#9AM", "#10AM","#11AM","#12PM","#1PM","#2PM","#3PM","#4PM","#5PM"];
-let milTime = [9,10,11,12,13,14,15,16,17]
-
-for (let i = 0; i < timeBlock.length; i++) {
-    savedEvent = $(".saved-event");
-
-    if (localStorage.getItem(timeBlock[i])) {
-        $(timeBlock[i]).val(localStorage.getItem(timeBlock[i]))
-    };
-};
-
-function hourStyle() {
+$(document).ready(function () {
+    var currentTime = moment().format("MMMM Do YYYY, H:mm:ss a");
     var currentHour = moment().hour();
-    for (let i=0; i< timeBlock.length; i++)
+    var currentDay = moment().format("MMMM Do, YYYY");
+    console.log(currentHour)
+    console.log(currentTime)
+    console.log(currentDay)
+    currentDay = moment().format("MMMM Do YYYY, H:mm:ss a");
+    $("#currentDay").text("Today's Date: " + currentDay)
 
-    if (milTime[i] ===currentHour) {
-        $(timeBlock[i]).addClass("present");
+    $(".saveBtn").on("click", function () {
+        var timeBlock = $(this).attr('data-value');
+        var userInput = $("#" + timeBlock).val();
+        localStorage.setItem("#" + timeBlock, userInput);
+        console.log(timeBlock)
+        console.log(userInput)
+    });
+    let Time = ["#hour-9", "#hour-10", "#hour-11", "#hour-12", "#hour-13", "#hour-14", "#hour-15", "#hour-16", "#hour-17"];
+    let milTime = [9, 10, 11, 12, 13, 14, 15, 16, 17]
+
+    for (let i = 0; i < Time.length; i++) {
+    
+        if (localStorage.getItem(Time[i])) {
+            $(Time[i]).val(localStorage.getItem(Time[i]))
+        };
+
+    };
+
+
+    function colorChanger() {
+        var currentHour = moment().hour();
+        for (let i = 0; i < Time.length; i++)
+
+            if (milTime[i] === currentHour) {
+                $(Time[i]).addClass("present");
+            }
+            else if (milTime[i] >= currentHour) {
+                $(Time[i]).addClass("future");
+            } else {
+                $(Time[i]).addClass("past")
+            };
     }
-      else if (milTime[i] >= currentHour) {
-          $(timeBlock[i]).addClass("future");
-      } else {
-          $(timeBlock[i]).addClass("past")
-      };  
-    }
-hourStyle();
+    colorChanger();
 })
